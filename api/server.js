a70e2c84e4f3e29e312065cedfe5001c6b45c910
Harmik Lathiya <lathiyaharmik50@gmail.com>
2023-07-22 11:59:55 +0530

    change the local url to live server url

--- api/server.js
+++ api/server.js
@@ -24,7 +24,12 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://finderwebapp.netlify.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
