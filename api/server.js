ca7a208db13d5bbd89909c78e649af758d972b2a
Harmik Lathiya <lathiyaharmik50@gmail.com>
2023-07-22 14:45:24 +0530

    changes in server.js file

--- api/server.js
+++ api/server.js
@@ -24,15 +24,25 @@ const connect = async () => {
   }
 };
 
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://finderwebapp.netlify.app"],
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(cookieParser());
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
+  next();
+});
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
